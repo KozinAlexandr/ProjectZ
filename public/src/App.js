e20c19d188abb911ee578ce4fa98b0fc98645125
Alexandr Kozin <sasha.kozin.2014alex444@ya.ru>
2022-01-14 00:37:20 +0300

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,33 @@
+import Header from './Components/Header'
+import Compet from './Components/Compet'
+import NavBar from './Components/NavBar'
+import Support from './Components/Support'
+import Expertise from './Components/Expertise'
+import Tariffs from './Components/Tariffs'
+import Cases from './Components/Cases'
+import Team from './Components/Team'
+import Partnership from './Components/Partnership'
+import Reviews from './Components/Reviews'
+import Footer from './Components/Footer'
+import Solution from './Components/Solution'
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <Header />
+    <Compet/>
+    <Support/>
+    <Expertise/>
+    <Tariffs />
+    <Solution />
+    <Cases />
+    <Team />
+    <Reviews />
+    <Partnership />
+    <Footer/>
+    </>
+  )
+}
+
+export default App;
